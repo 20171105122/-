20672f665d5c6c0e52273f74ba178dcba577280a
20171105122 <20171105122a@admindeimac-38.local>
2018-06-27 10:30:19 +0800

    Initial Commit

---
+++ 评分系统/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  评分系统
+//
+//  Created by 冯佩玲 on 18/6/27.
+//  Copyright © 2018年 冯佩玲. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
